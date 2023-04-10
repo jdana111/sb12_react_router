a0d895e3afa863660b7651fa55f66bf0c6201026
John Dana <john@danasd.com>
2023-04-10 13:01:35 -0600

    Contact route temporarily as a peer route to Root

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from "./routes/root";
 import ErrorPage from './error-page';
+import Root from "./routes/root";
+import Contact from "./routes/contact";
 import './index.css'
 
 const router = createBrowserRouter([
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
   },
+  {
+    path: "/contacts/:contactId",
+    element: <Contact />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
